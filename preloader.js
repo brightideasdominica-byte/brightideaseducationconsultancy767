ca0104bce67a9380531f5b8c060db5bef9745e5e
brightideasdominica-byte <brightideasdominica@gmail.com>
2025-12-10 07:17:54 -0400

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,7 +1,6 @@
  const preloader = document.getElementById('preloader');
 
+  // Hide preloader after 2 seconds
   setTimeout(() => {
     preloader.classList.add('hide');
-
-  }, 1000);
-
+  }, 0.1);
